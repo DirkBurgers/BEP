4c28ba67974b7f7e20e48ace3b1be044c8e8b2c0
Dirk <d.burgers@student.tue.nl>
2024-03-08 11:09:28 +0100

    Updated orientation plot

--- Experiments/experimentplots.jl
+++ Experiments/experimentplots.jl
@@ -2,7 +2,7 @@ using LinearAlgebra
 using Makie, CairoMakie
 
 set_theme!(theme_latexfonts()) # Theme for plots 
-update_theme!(Theme(fontsize = 18)) # 36
+update_theme!(Theme(fontsize = 36)) # 18 for testing and 36 for report
 
 function createlineplot!(ax::Axis, nn::MyTwoLayerNN.TwoLayerNN, td::MyTwoLayerNN.TrainingData)
     xmin, xmax = td.x |> Iterators.flatten |> extrema
@@ -11,10 +11,10 @@ function createlineplot!(ax::Axis, nn::MyTwoLayerNN.TwoLayerNN, td::MyTwoLayerNN
     x = range(xmin, xmax, length=100)
     y = [MyTwoLayerNN.forward(nn, p) for p in x]
 
-    lines!(ax, x, y, color=:darkorange2)
+    lines!(ax, x, y, color=:darkorange2, linewidth=3)
     
     # Plot the points
-    scatter!(ax, vcat(td.x...), td.y)
+    scatter!(ax, vcat(td.x...), td.y, markersize=16)
 end
 
 function createorientiationplot!(ax::Axis, nni, nnt)
@@ -33,32 +33,15 @@ function createorientiationplot!(ax::Axis, nni, nnt)
     ori_trained = angle.(vec(nnt.w) + im * nnt.b)
     
     # Plot all points
-    scat_init = scatter!(ax, ori_initial, amp_initial)
-    scat_trained = scatter!(ax, ori_trained, amp_trained, color=:darkorange2)
+    scat_init = scatter!(ax, ori_initial, amp_initial, markersize=16)
+    scat_trained = scatter!(ax, ori_trained, amp_trained, markersize=16, color=:darkorange2)
     
     axislegend(ax, [scat_init, scat_trained], ["Before training", "After training"])
 end
 
 function createorientiationplot(nni, nnt)
-    # Initialize figure 
     f = Figure()
-    ax = Axis(f[1, 1], xticks=-3:1:3, xlabel="Orientation", ylabel="Amplitude")
-    
-    xlims!(ax, -π, π)
-    
-    # Calculate amplitude
-    amp_initial = abs.(nni.a) .* norm.(eachrow([nni.w nni.b]))
-    amp_trained = abs.(nnt.a) .* norm.(eachrow([nnt.w nnt.b]))
-    
-    # Calculate orientation
-    ori_initial = angle.(vec(nni.w) + im * nni.b)
-    ori_trained = angle.(vec(nnt.w) + im * nnt.b)
-    
-    # Plot all points
-    scat_init = scatter!(ax, ori_initial, amp_initial, strokewidth=0.5)
-    scat_trained = scatter!(ax, ori_trained, amp_trained, color=:darkorange2, strokewidth=0.5)
-    
-    axislegend(ax, [scat_init, scat_trained], ["Before training", "After training"])
-    
+    ax = Axis(f[1, 1])
+    createorientiationplot!(ax, nni, nnt)
     return f
 end
--- Test/Test.jl
+++ Test/Test.jl
@@ -1,31 +1,32 @@
-using BenchmarkTools
+using BenchmarkTools, JLD2
 
 include("../MyTwoLayerNN/MyTwoLayerNN.jl")
-include("../MyTwoLayerNN/plotsNN.jl")
+# include("../MyTwoLayerNN/plotsNN.jl")
 
 import .MyTwoLayerNN
 
 function main()
     # Create the NN
     d = 1       # Dimension data
-    m = 100_000    # Number of hidden neurons
-    γ = 0.5
-    γ′ = 0.0
-    myNN = MyTwoLayerNN.TwoLayerNN(d, m, γ, γ′;σ=MyTwoLayerNN.LeakyReLU(0.1))
+    m = 1000    # Number of hidden neurons
+    γ = 2.0
+    γ′ = -1.0
+    myNN = MyTwoLayerNN.TwoLayerNN(d, m, γ, γ′)
 
     # Create training data 
     n = 4
     dataX = [[-1/2], [-1/6], [1/6], [1/2]]
-    dataY = [0.25, 0.03, 0.03, 0.25]
+    dataY = [1/4, 1/30, 1/30, 1/4]
     # dataX = [[-1/2, -1/2], [-1/3, -1/3], [1/3, 1/3], [1/2, 1/2]]
     # dataY = [0.25, 0.03, 0.03, 0.25]
 
-    learning_rate = 0.1 # 0.01 for γ 0.5, 1.0 and 1.0 for γ = 1.75
-    steps = 800_000
+    learning_rate = 10_000.0 # 0.01 for γ 0.5, 1.0 and 1.0 for γ = 1.75
+    steps = 100_000_000
     myTrainingData = MyTwoLayerNN.TrainingData(dataX, dataY, learning_rate, steps)
 
     # Create copy of initial weights and biases
-    initParms = (a = copy(myNN.a), w = copy(myNN.w), b = copy(myNN.b))
+    initNN = MyTwoLayerNN.copy(myNN)
+    # initParms = (a = copy(myNN.a), w = copy(myNN.w), b = copy(myNN.b))
 
     # Train
     @time MyTwoLayerNN.train!(myNN, myTrainingData; debug=true)
@@ -37,8 +38,8 @@ function main()
 
     # View result
     MyTwoLayerNN.summary(myNN)
-    plotNN(myNN, dataX, dataY, -0.5:0.01:0.5)
-    orientationPlot(initParms, myNN)
+    # plotNN(myNN, dataX, dataY, -0.5:0.01:0.5)
+    # orientationPlot(initNN, myNN)
 end;
 
 main()
