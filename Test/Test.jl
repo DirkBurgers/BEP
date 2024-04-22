e495f6bb5c1ebe77728494df4adfc76eed6f5f1d
Dirk <d.burgers@student.tue.nl>
2024-04-22 17:16:07 +0200

    Neuron trajectories pictures

--- Test/Test.jl
+++ Test/Test.jl
@@ -1,6 +1,6 @@
 using BenchmarkTools, JLD2
 
-include("../MyTwoLayerNN/MyTwoLayerNN.jl")
+# include("../MyTwoLayerNN/MyTwoLayerNN.jl")
 # include("../MyTwoLayerNN/plotsNN.jl")
 
 import .MyTwoLayerNN
@@ -8,10 +8,10 @@ import .MyTwoLayerNN
 function main()
     # Create the NN
     d = 1       # Dimension data
-    m = 1000    # Number of hidden neurons
-    γ = 2.0
-    γ′ = -1.0
-    myNN = MyTwoLayerNN.TwoLayerNN(d, m, γ, γ′)
+    m = 1_000    # Number of hidden neurons
+    γ = 1.5
+    γ′ = -0.5
+    myNN = MyTwoLayerNN.TwoLayerNN(d, m, γ, γ′; symmetric = true)
 
     # Create training data 
     n = 4
@@ -20,8 +20,8 @@ function main()
     # dataX = [[-1/2, -1/2], [-1/3, -1/3], [1/3, 1/3], [1/2, 1/2]]
     # dataY = [0.25, 0.03, 0.03, 0.25]
 
-    learning_rate = 10_000.0 # 0.01 for γ 0.5, 1.0 and 1.0 for γ = 1.75
-    steps = 100_000_000
+    learning_rate = 4000.0 # 0.01 for γ 0.5, 1.0 and 1.0 for γ = 1.75
+    steps = 1_000_000
     myTrainingData = MyTwoLayerNN.TrainingData(dataX, dataY, learning_rate, steps)
 
     # Create copy of initial weights and biases
