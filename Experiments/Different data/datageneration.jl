41fda9119abe0253fadf4683097ae8b5aea17b65
Dirk <d.burgers@student.tue.nl>
2024-05-25 13:42:02 +0200

    Added Manifest + Prokect.toml and updated some notebooks

--- Experiments/Different data/datageneration.jl
+++ Experiments/Different data/datageneration.jl
@@ -3,7 +3,7 @@ using JLD2
 include("../../MyTwoLayerNN/MyTwoLayerNN.jl")
 
 # Create DATA_FOLDER
-DATA_FOLDER = joinpath(@__DIR__, "leaky relu")
+DATA_FOLDER = joinpath(@__DIR__, "phase diagram")
 if !isdir(DATA_FOLDER)
     mkdir(DATA_FOLDER)
 end
@@ -16,17 +16,20 @@ dataY = [1/4, 1/30, 1/30, 1/4]
 # NN parameters
 m = 1_000
 γ = 1.5
-γ′ = -0.5
+γ′ = 0.5
+# [1.5, 0.5, 0.5]
+# [0.0, 0.0, 0.000001], [0.5, 0.0, 0.0005], [1.0, 0.0, 0.5], [1.5, 0.0, 25]
+# [0.5 -0.5, 0.0005], [1.0, -0.5, 5], [1.5, -0.5, 1000]
 
 # Training parameters
-learning_rate = 1000.0
+learning_rate = 0.5
 max_steps = 100_000_000
 
 training_data = MyTwoLayerNN.TrainingData(dataX, dataY, learning_rate, max_steps)
 
-function simulationstep(training_data, leaky_constant)
+function simulationstep(training_data)
     # Create the NN
-    nn = MyTwoLayerNN.TwoLayerNN(d, m, γ, γ′; σ=MyTwoLayerNN.LeakyReLU(leaky_constant))
+    nn = MyTwoLayerNN.TwoLayerNN(d, m, γ, γ′)
 
     # Create copy of inital weights 
     initialNN = MyTwoLayerNN.copy(nn)
@@ -34,9 +37,9 @@ function simulationstep(training_data, leaky_constant)
     # Train the NN
     MyTwoLayerNN.train!(nn, training_data; debug=true)
 
-    jldsave(joinpath(DATA_FOLDER, "run c=$leaky_constant.jld2"); initNN=initialNN, trainedNN=nn, trainingData=training_data)    
+    jldsave(joinpath(DATA_FOLDER, "run g=$γ gp=$γ′.jld2"); initNN=initialNN, trainedNN=nn, trainingData=training_data)    
 end
 
-for c ∈ [0.99]
-    simulationstep(training_data, c)
-end
+# for c ∈ [0.99]
+    simulationstep(training_data)
+# end
